3e1a2f22c19fabf470494ef389af0080c5d756e5
patrickgeorge1 <vtgaura@gmail.com>
2019-08-27 22:54:16 +0300

    + MessengerBundle

---
+++ src/MessengerBundle/Command/SocketCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\MessengerBundle\Command;
+
+
+use App\MessengerBundle\Sockets\Chat;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SocketCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('sockets:start-chat')
+            // the short description shown while running "php bin/console list"
+            ->setHelp("Starts the chat socket demo")
+            // the full command description shown when running the command with
+            ->setDescription('Starts the chat socket demo')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Chat socket',// A line
+            '============',// Another line
+            'Starting chat, open your browser.',// Empty line
+        ]);
+
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new Chat()
+                )
+            ),
+            8080
+        );
+
+        $server->run();
+    }
+}
